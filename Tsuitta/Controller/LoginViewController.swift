8a11b48f65b84209d90e837edb94d4855ac474e7
domon <rd050201@gmail.com>
2015-12-05 17:02:12 +0900

    issue #1　Main.storyboardで最初に表示されるVCを設定。（なんか初期設定ミスっていて読み込まれなかったので...

--- Tsuitta/Controller/LoginViewController.swift
+++ Tsuitta/Controller/LoginViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
