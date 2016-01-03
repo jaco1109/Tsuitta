04fbd702e39957fce74d4143a8b353d9b70b44b6
domon <rd050201@gmail.com>
2016-01-03 17:24:06 +0900

    #16　GetAPIのテスト用にいろいろ書きました。

--- Tsuitta/Controller/TimeLineViewController.swift
+++ Tsuitta/Controller/TimeLineViewController.swift
@@ -1,20 +1,78 @@
-//
-//  TimeLineViewController.swift
-//  Tsuitta
-//
-//  Created by domon on 2015/12/05.
-//  Copyright © 2015年 土門良輔. All rights reserved.
-//
-
-import Foundation
 import UIKit
+import TwitterKit
 
-class TimeLineViewController : UIViewController {
+class TimeLineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var tableView: UITableView!
+    var tweets: [TWTRTweet] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    var prototypeCell: TWTRTweetTableViewCell?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView = UITableView(frame: self.view.bounds)
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        print("fiusdbudsbf")
+        prototypeCell = TWTRTweetTableViewCell(style: .Default, reuseIdentifier: "cell")
+        
+        tableView.registerClass(TWTRTweetTableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(tableView)
+        
+        loadTweets()
+        
+        GetAPIManager.sharedInstance.searchUser("Twitter API") {
+            users in
+            users.forEach({ (user) -> () in
+                print("-------------")
+                print(user)
+            })
+        }
+        
+        self.navigationController?.navigationBarHidden = false
+        
+        //self.navigationController?.navigationBar.translucent = false
     }
     
+    func loadTweets() {
+        GetAPIManager.sharedInstance.searchImage("d") {
+        tweets in
+            for tweet in tweets {
+                self.tweets.append(tweet)
+            }
+        }
+        GetAPIManager.sharedInstance.tweet("678556859976933376", callback: { (tweets) -> Void in
+            self.tweets.append(tweets)
+        })
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of Tweets.
+        return tweets.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! TWTRTweetTableViewCell
+        
+        let tweet = tweets[indexPath.row]
+        cell.configureWithTweet(tweet)
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let tweet = tweets[indexPath.row]
+        
+        prototypeCell?.configureWithTweet(tweet)
+        
+        if let height = prototypeCell?.calculatedHeightForWidth(self.view.bounds.width) {
+            return height
+        } else {
+            return tableView.estimatedRowHeight
+        }
+    }
 }
