af1d3d84249134e7ee25da4e30dcc6c974cc7979
domon <rd050201@gmail.com>
2016-01-03 17:01:23 +0900

    #16　ユーザー情報取得GetAPI作成したよ。

--- Tsuitta/Model/API/GetAPIManager.swift
+++ Tsuitta/Model/API/GetAPIManager.swift
@@ -58,4 +58,51 @@ class GetAPIManager {
         store.logOutUserID(userID)
     }
     
+    //MARK: UserData
+    
+    //TODO: とりあえずここに書いていますが、後でちゃんと書き直します。
+    struct TWTRCoreUser{
+        private(set) var userID: String
+        private(set) var name: String
+        private(set) var screenName: String
+        private(set) var profileImageURL: String
+        
+        init(userData: TWTRUser){
+            userID = userData.userID
+            name = userData.name
+            screenName = userData.screenName
+            profileImageURL = userData.profileImageURL
+        }
+    }
+    
+    //TODO: とりあえず毎回requestを送ってdataを取得します。
+    //そのうち、dataクラスを作成して、初回のみrequestを飛ばしてそれ以外はdataクラスから必要なデータのみ取得します。
+    func userData(callback: (TWTRCoreUser?, NSError?) -> Void){
+        guard let userID = userID() else {
+            debug("ログインされてないでえ")
+            return
+        }
+        
+        client.loadUserWithID(userID) { (user, error) -> Void in
+            if let u = user {
+                callback(TWTRCoreUser(userData: u), error)
+            } else {
+                debug("えらーだお")
+            }
+        }
+    }
+    
+    //TODO: とりあえず毎回requestを送ってdataを取得します。
+    //そのうち、dataクラスを作成して、初回のみrequestを飛ばしてそれ以外はdataクラスから必要なデータのみ取得します。
+    func profileData(callback: TWTRLoadUserCompletion){
+        guard let userID = userID() else {
+            debug("ログインされてないでえ")
+            return
+        }
+        
+        client.loadUserWithID(userID) { (user, error) -> Void in
+            callback(user, error)
+        }
+    }
+    
 }
