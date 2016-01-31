621fc9c813c10064f565317b87740a131a5cd608
domon <rd050201@gmail.com>
2016-01-31 14:14:17 +0900

    #33  TweetDetailViewを作成しました。TweetDetailVCから呼び出され、画面のなんやかんやをいろいろやります。
    とりあえず、今はTableViewだけ持っていて、cellには、mainのツイートのセルを1つ(すでにコードで書いている部分)、その下にリプのセルを追加していく予定

---
+++ Tsuitta/View/Tweet/TweetDetailView.swift
@@ -0,0 +1,49 @@
+import UIKit
+import TwitterKit
+
+class TweetDetailView: UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    private var mainTweetData: TWTRTweet?
+    
+    @IBOutlet weak private var tableView: UITableView!{
+        didSet{
+            self.tableView.delegate = self
+            self.tableView.dataSource = self
+            
+            self.tableView.estimatedRowHeight = 200
+            self.tableView.rowHeight = UITableViewAutomaticDimension
+        }
+    }
+    
+    private func createMainTweetDetailTableViewCell() -> MainTweetDetailTableViewCell? {
+        guard let tweet = mainTweetData else{
+            return nil
+        }
+        
+        guard let cell = UINib(nibName: "MainTweetDetailTableViewCell", bundle: nil).instantiateWithOwner(self, options: nil).first as? MainTweetDetailTableViewCell else {
+            return nil
+        }
+        
+        cell.selectionStyle = .None
+        cell.setup(tweet)
+        
+        
+        return cell
+    }
+    
+    func reloadMainTweetData(tweetData: TWTRTweet) {
+        self.mainTweetData = tweetData
+        
+        self.tableView.reloadData()
+    }
+    
+    //MARK: TableView
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return createMainTweetDetailTableViewCell() ?? UITableViewCell()
+    }
+}
