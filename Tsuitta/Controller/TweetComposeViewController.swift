d2780e608f415019e6254b874b18ddb0e013267d
domon <rd050201@gmail.com>
2016-01-31 14:38:00 +0900

    #35  ツイート投稿画面のVCに、とりあえずViewを作成してloadViewで読み込むようにした

--- Tsuitta/Controller/TweetComposeViewController.swift
+++ Tsuitta/Controller/TweetComposeViewController.swift
@@ -3,4 +3,11 @@ import UIKit
 /// ツイート投稿画面のVC
 class TweetComposeViewController: UIViewController {
     
+    override func loadView() {
+        super.loadView()
+        
+        if let view = UINib(nibName: "TweetComposeView", bundle: nil).instantiateWithOwner(self, options: nil).first as? TweetComposeView {
+            self.view = view
+        }
+    }
 }
