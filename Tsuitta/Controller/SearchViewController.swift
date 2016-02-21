d023314f664278c13621a96c0eb85dbf42d44676
domon <rd050201@gmail.com>
2016-02-21 19:00:01 +0900

    #39  VCのloadViewでViewを読み込むよ

--- Tsuitta/Controller/SearchViewController.swift
+++ Tsuitta/Controller/SearchViewController.swift
@@ -1,5 +1,13 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
+        
+        if let view = UINib(nibName: "SearchView", bundle: nil).instantiateWithOwner(self, options: nil).first as? SearchView {
+            view.delegate = self
+            self.view = view
+        }
+    }
     
 }
