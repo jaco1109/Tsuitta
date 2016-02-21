c4892914369241ad3c8449bde3cc0a53b2f9d429
domon <rd050201@gmail.com>
2016-02-21 19:05:51 +0900

    #39  request系の処理を実装したよよよよおよよよよｙ

--- Tsuitta/Controller/SearchViewController.swift
+++ Tsuitta/Controller/SearchViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
+import TwitterKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, SearchHeaderTableViewCellDelegate {
+    
     override func loadView() {
         super.loadView()
         
@@ -8,6 +10,20 @@ class SearchViewController: UIViewController {
             view.delegate = self
             self.view = view
         }
+    }
+    
+    //MARK: SearchHeaderTableViewCellDelegate
+    
+    func requestSearchTweet(word: String, callback: ([TWTRTweet]) -> Void) {
+        APILocator.sharedInstance.search.tweets(word, callback: callback)
+    }
+    
+    func requestSearchUser(word: String, callback: ([TWTRUser]) -> Void) {
+        APILocator.sharedInstance.search.users(word, callback: callback)
+    }
+    
+    func requestSearchImage(word: String, callback: ([TWTRTweet]) -> Void) {
+        APILocator.sharedInstance.search.images(word, callback: callback)
     }
     
 }
