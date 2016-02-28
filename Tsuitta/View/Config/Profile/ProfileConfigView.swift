f80c271d4fa40e715e5b11d686670582393e5eff
domon <rd050201@gmail.com>
2016-02-28 17:46:42 +0900

    #41  プロフィール変更画面の項目のEnumを作成

--- Tsuitta/View/Config/Profile/ProfileConfigView.swift
+++ Tsuitta/View/Config/Profile/ProfileConfigView.swift
@@ -1,4 +1,45 @@
 import UIKit
+enum ProfileConfigItem: Int {
+    case Image = 0
+    case Name
+    case Description
+    case Location
+    case Url
+    
+    func title() -> String {
+        switch self {
+        case .Image:
+            return ""
+        case .Name:
+            return "名前"
+        case .Description:
+            return "説明"
+        case .Location:
+            return "場所"
+        case .Url:
+            return "Webサイト"
+        }
+    }
+    
+    func defaultValue(data: ProfileDetailData) -> String {
+        switch self {
+        case .Image:
+            return ""
+        case .Name:
+            return data.name
+        case .Description:
+            return data.description
+        case .Location:
+            return data.location
+        case Url:
+            return data.url
+        }
+    }
+    
+    static func count() -> Int {
+        return 5
+    }
+}
 
 class ProfileConfigView: UIView, UITableViewDelegate, UITableViewDataSource {
     
