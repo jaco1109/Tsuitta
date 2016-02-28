3da750ab21e0abecb3b2e97a405b52d13ae7b24a
domon <rd050201@gmail.com>
2016-02-28 16:01:10 +0900

    #41  プロフィール変更画面を表示したらプロフィール情報を取得し、現時点のプロフィール情報を表示するお

--- Tsuitta/Controller/ProfileConfigViewController.swift
+++ Tsuitta/Controller/ProfileConfigViewController.swift
@@ -8,4 +8,20 @@ class ProfileConfigViewController: UIViewController {
             self.view = view
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let stringID = APILocator.sharedInstance.user.id(), id = Int(stringID) else{
+            return
+        }
+        
+        APILocator.sharedInstance.user.profileDetailData(id) { data in
+            guard let view = self.view as? ProfileConfigView else{
+                return
+            }
+            
+            view.setup(data)
+        }
+    }
 }
