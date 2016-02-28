5e8bf544ad8b28a0471bff89b85a593899121ccb
domon <rd050201@gmail.com>
2016-02-28 14:06:19 +0900

    #41  設置画面のVCのloadViewでConfigViewを呼び出すようにする

--- Tsuitta/Controller/ConfigViewController.swift
+++ Tsuitta/Controller/ConfigViewController.swift
@@ -1,5 +1,11 @@
 import UIKit
 
 class ConfigViewController: UIViewController {
-    
+    override func loadView() {
+        super.loadView()
+        
+        if let view = UINib(nibName: "ConfigView", bundle: nil).instantiateWithOwner(self, options: nil).first as? ConfigView {
+            self.view = view
+        }
+    }
 }
